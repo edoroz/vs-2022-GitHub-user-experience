244d6d6cb142478b9ee318b25771e12d8239a3c8
Edgar Oroz <eoroz@indconsulting.com>
2022-12-30 12:12:43 -0700

    Add project files.

---
+++ vs-2022-GitHub-user-experience/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace vs_2022_GitHub_user_experience
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ vs-2022-GitHub-user-experience/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vs_2022_GitHub_user_experience.MvcApplication" Language="C#" %>
